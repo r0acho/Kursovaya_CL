2b6c49f9c428b0b7b11b4af940c1ef7ff68c631a
r0acho <ivan.linzov@gmail.com>
2022-01-07 16:59:42 +0300

    commit webui, parser and get_arcticles script

---
+++ 1st student(parser)/get_arcticles.py
@@ -0,0 +1,16 @@
+from pymongo import MongoClient
+import re
+
+client = MongoClient("PUT YOUR CONNECTION STRING HERE")
+database = client.get_database("prasim")
+collection = database["news"]
+
+db_documents = list(collection.find({}))
+
+with open('Arcticles.txt', 'w', encoding="utf-8") as f:
+    for i in range(len(list(db_documents))):
+        f.write(db_documents[i]['headline'])
+        f.write('\n')
+        f.write(re.sub(r'\', \'', ' ', str(db_documents[i]['text'])))
+        f.write('\n\n')
+    f.close()
